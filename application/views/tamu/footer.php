5c01123d6bfea970445f9c054fa091dde8c363a6
yuliafamurina0306 <ramadestiana07@gmail.com>
2022-03-26 11:15:15 +0700

    project pertama

---
+++ application/views/tamu/footer.php
@@ -0,0 +1,12 @@
+<!-- Sticky Footer -->
+<footer >
+  <div class="container mx-auto">
+    <div class="copyright text-center my-auto">
+   
+    <span>Copyright © JAPATI SUHUNAN <marquee bgcolor="orange" behavior="gerakan" loop ="10" direction="right" height="30px">Ayo Nikmati Hunian Anda Sekarang Juga.</marquee> </span> 
+    <a href=""></a>
+     
+      </div>
+    </div>
+  </div>
+</footer>
